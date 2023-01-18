66f1c4a89a5e9d54d97e8af0b66609c34440a0d5
BatoulMDarwish <batoulmahmoud687@gmail.com>
2023-01-18 18:24:03 +0300

    first commit

---
+++ lib/core/strings/failurs.dart
@@ -0,0 +1,5 @@
+const String SERVER_FAILURE_MESSAGE = 'please try again later .';
+
+const String EMPTY_CACHE_FALURE_MESSAGE = 'No Data';
+
+const String oFFLINE_FAILURE_MESSAGE = 'please Check your Internet Connection';
